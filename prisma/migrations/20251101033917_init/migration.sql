635e32fec8e443e6eca36004655749280336d11c
Djiga Salane <djiga2015@gmail.com>
2025-11-02 04:13:47 +0000

    daxx

--- prisma/migrations/20251101033917_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Tarnsfert" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Tarnsfert_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20251101171729_deploy/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[ref]` on the table `Tarnsferts` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Tarnsferts_ref_key" ON "Tarnsferts"("ref");
