a71b0ae29a990b67c07a7ba89ce215005118827f
Djiga Salane <djiga2015@gmail.com>
2025-11-03 01:45:23 +0000

    devlop(audit):integrer service audit au module transfert

--- src/common/helper/utils.ts
+++ src/common/helper/utils.ts
@@ -1,6 +1,6 @@
-import { TRANSFERT_STATUS } from "@prisma/client";
+import { TRANSFER_STATUS } from "@prisma/client";
 
-export const finalTransfertStatus = [TRANSFERT_STATUS.FAILED, TRANSFERT_STATUS.CANCELED, TRANSFERT_STATUS.SUCCESS] as TRANSFERT_STATUS[]
+export const finalTransfertStatus = [TRANSFER_STATUS.FAILED, TRANSFER_STATUS.CANCELED, TRANSFER_STATUS.SUCCESS] as TRANSFER_STATUS[]
 export const firstnames = ['Mamadou', 'Moussa', 'Fatoumata', 'Mariane', 'Khadija'];
 export const lastnames = ['Diouf', 'Ndiaye', 'Diop', "Ndong", 'Fall']
 export const prefix = [77, 78, 76, 71, 33]
